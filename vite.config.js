2871c1db1026ce48e5b7db6a31f3581812341746
Ty Thomasson <thomassonty@gmail.com>
2025-09-07 19:16:25 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,9 @@ export default defineConfig({
     host: '0.0.0.0',  // exposes to LAN
     port: 5173,       // optional, default is 5173
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
-
